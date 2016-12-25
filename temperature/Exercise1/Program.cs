9637d2b6a3499221527bc931b80fae340335f8f7
Mohamad Farhan bin Zainudin <paan.zainudin97@gmail.com>
2016-12-25 16:12:31 +0800

    Add files via upload

---
+++ temperature/Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Temperature
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the temperature right now?");
+            int temperature = Convert.ToInt32(Console.ReadLine());
+            if (temperature < 20)
+            {
+                Console.WriteLine("It is cold!");
+            }
+            else if (temperature >= 20 & temperature <= 35)
+            {
+                Console.WriteLine("Nice day!");
+            }
+            else if (temperature > 35)
+            {
+                Console.WriteLine("It is hot!");
+            }
+            Console.ReadLine ();
+        }
+    }
+}
